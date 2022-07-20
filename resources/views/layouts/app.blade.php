7f1675a2676d4185eb4d84cb62eba67de8142167
mohammedterfa <mohammedterfa@gmail.com>
2022-07-20 12:50:16 +0200

    solve some bugs with laravel vite

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,14 +4,14 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-
         <title>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
-
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+        <script src="js/app.js"></script>
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
@@ -29,5 +29,7 @@
                 {{ $slot }}
             </main>
         </div>
+
+
     </body>
 </html>
