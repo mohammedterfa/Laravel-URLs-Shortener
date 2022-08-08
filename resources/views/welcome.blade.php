08acc267eadb299ae57f68689a1b8ba6e2da670e
mohammedterfa <mohammedterfa@gmail.com>
2022-08-08 15:26:49 +0200

    short url working now

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,11 +3,16 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 flex justify-center bg-white border-b border-gray-200">
-                    <form method="POST" action="{{ route('short.url') }}">
-                        @csrf
-                        <input type="url" name="original_url">
-                        <button type="submit">Short</button>
-                    </form>
+                    <section>
+                        <h1 class="text-4xl text-blue-800">Short your link</h1>
+
+                        <form method="POST" action="{{ route('short.url') }}">
+                            @csrf
+                            <input class="border border-gray-300 rounded-lg" type="url" name="original_url">
+                            <button class="m-2 px-6 py-2 bg-green-500 hover:bg-green-700 rounded-lg" type="submit">Short</button>
+                        </form>
+                    </section>
+
                 </div>
             </div>
         </div>
