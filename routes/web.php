b08c34daf25be3a322ef0fdc75098147943dbfd4
mohammedterfa <mohammedterfa@gmail.com>
2022-09-14 14:50:33 +0200

    FINISH

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ShortUrlController;
+use App\Http\Controllers\ShortUserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -24,6 +25,7 @@ Route::get('/dashboard', function () {
 
 require __DIR__.'/auth.php';
 
+Route::get('/links', [ShortUserController::class, 'index'])->name('user.links')->middleware('auth');
 Route::post('/short', [ShortUrlController::class, 'short'])->name('short.url');
 Route::get('/{code}', [ShortUrlController::class, 'show'])->name('short.show');
-Route::get('/links', [ShortUrlController::class, 'index'])->name('user.links');
+
